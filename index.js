80dc551b07b82e637b09072468f141cd121191d2
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-14 16:29:57 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
-}
+    // Define the function that will handle the event
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Select the element by ID
+    const input = document.getElementById('button');
+  
+    // Attach a click event listener to the element
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the addingEventListener function to set up the event listener
+  addingEventListener();
+  
+
